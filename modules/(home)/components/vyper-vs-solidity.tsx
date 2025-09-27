065056926f90fb4c08531ff5353ac919b7770c31
Ye Yu <nearjilt@gmail.com>
2025-09-27 17:15:16 +0800

    feat(home): add basic text content

---
+++ modules/(home)/components/vyper-vs-solidity.tsx
@@ -0,0 +1,13 @@
+export default function VyperVsSolidity() {
+  return (
+    <div className="mx-auto w-full flex-1">
+      <header className="flex flex-col items-center justify-center gap-2">
+        <h3 className="text-2xl font-semibold">探索 Vyper 优势</h3>
+        <p className="">
+          Vyper
+          的设计理念并非仅限于理论。了解其对清晰度、安全性和效率的关注如何在实践中转化为更简洁、更安全、更高效的代码。
+        </p>
+      </header>
+    </div>
+  );
+}
--- modules/(home)/index.tsx
+++ modules/(home)/index.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./components/hero-section";
+import VyperVsSolidity from "./components/vyper-vs-solidity";
 
 export default function HomeModule() {
   return (
-    <div className="flex flex-1">
+    <div className="m-auto flex min-h-screen flex-1 flex-col gap-12">
       <HeroSection />
+      <VyperVsSolidity />
     </div>
   );
 }
